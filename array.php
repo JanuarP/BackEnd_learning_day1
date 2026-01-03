f4851b9e8c83441db4bdab7c636ef84e487c773a
JanuarP <yanuardwiputrawarsito@gmail.com>
2026-01-03 11:50:46 +0700

    first commit

---
+++ array.php
@@ -0,0 +1,10 @@
+<?php
+  // Version 1
+  $months = array("January", "February", "March");
+ 
+  // Version 2
+  $days = ["Monday", "Tuesday", "Wednesday"];
+
+  echo "My favorite month is " . $months[0] . ".<br>"; // Output: My favorite month is January
+  echo "My favorite day is " . $days[2] . ".";         // Output: My favorite day is Wednesday
+?>
---
+++ hello.php
@@ -0,0 +1,48 @@
+<?php
+    // $number = 42;                     // Integer
+    // $value = 3.14;                    // Float
+    // $system = true;                   // Boolean
+    // $teks = "PHP itu keren banget!";  // String
+
+    // echo $teks; // Output: PHP itu keren banget!
+
+    // $empty = null;
+    // echo $empty;
+
+//     $number1 = 10;
+//     $number2 = 5;
+//   $addition = $number1 + $number2;        // Penjumlahan
+//   $subtraction = $number1 - $number2;     // Pengurangan
+//   $multiplication = $number1 * $number2;  // Perkalian
+//   $division = $number1 / $number2;        // Pembagian
+//   $remainder = $number1 % $number2;       // Sisa Bagi
+
+//   echo "Hasil penjumlahan: $addition <br>";     // Output: 15 
+//   echo "Hasil pengurangan: $subtraction <br>";  // Output: 5 
+//   echo "Hasil perkalian: $multiplication <br>"; // Output: 50
+//   echo "Hasil pembagian: $division <br>";       // Output: 2
+//   echo "Hasil sisa bagi: $remainder <br>";      // Output: 0
+
+// $number1 = 10;
+//   $number2 = 5;
+
+//   $greaterThan = $number1 > $number2 ? "Yes" : "No";  
+//   $lessThan = $number1 < $number2 ? "Yes" : "No";     
+//   $equalTo = $number1 == $number2 ? "Yes" : "No";     
+
+//   echo "Lebih besar dari: $greaterThan<br>"; // Output: Yes
+//   echo "Lebih kecil dari: $lessThan<br>";    // Output: No
+//   echo "Sama dengan: $equalTo";              // Output: No
+
+// $a = true;
+//   $b = false;
+
+//   echo ($a && $b);  // Output: (false)
+//   echo ($a || $b);  // Output: (true)
+//   echo (!$a);       // Output: (false)
+//   echo ($a xor $b); // Output: (true)
+
+//$a = 10 + 5; //10+5 adalah expression karena menghasilkan nilai suatu nilai yaitu 15
+
+print("Hello, World!"); // "Hello, World!" adalah nilai yang dihasilkan dari expression di sebelah kiri
+?>
